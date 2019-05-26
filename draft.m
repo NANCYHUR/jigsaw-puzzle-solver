fd37617fa7c4ac81709caf4e9ead73e079303fdb
nancy <hanghur@gmail.com>
2019-05-26 23:04:22 +1000

    successssss

--- draft.m
+++ draft.m
@@ -12,7 +12,7 @@ for ii=1:36
 end
 
 %% concat into 6 long pieces
-long_pieces = zeros(145, 216*6, 3, 6);
+six_pieces = zeros(145, 216*6, 3, 6);
 for n = 1:6
     img_first = squeeze(all_images(:, :, :,1));
     all_images(:, :, :,1) = [];    % remove the chosen image
@@ -31,10 +31,6 @@ for n = 1:6
             left_dist_i_2 = M_plus_S(left_vec, vec_i_r,1);
             right_dist_i_1 = M_plus_S(right_vec, vec_i_l,1);
             right_dist_i_2 = M_plus_S(right_vec, flip(vec_i_r,2),1);
-    %         left_dist_i_1 = SSDfunction(left_vec, flip(vec_i_l,2));
-    %         left_dist_i_2 = SSDfunction(left_vec, vec_i_r);
-    %         right_dist_i_1 = SSDfunction(right_vec, vec_i_l);
-    %         right_dist_i_2 = SSDfunction(right_vec, flip(vec_i_r,2));
             dist(i, 1) = left_dist_i_1;
             dist(i, 2) = left_dist_i_2;
             dist(i, 3) = right_dist_i_1;
@@ -42,7 +38,6 @@ for n = 1:6
         end
 
         threshold = 100000000000;
-    %     threshold = 10000;
         dist(dist>threshold) = NaN;
         dist(dist<=0) = NaN;
         if size(find(isnan(dist)),1)~=(size(dist,1)*size(dist,2))   % this matrix has valid values
@@ -66,7 +61,56 @@ for n = 1:6
             end
         end
     end
-    figure;
-    imshow(img_long_piece);
+    six_pieces(:,:,:,n) = img_long_piece;
+%     figure;
+%     imshow(img_long_piece);
 end
-imshow(img_long_piece);
+
+%% whole image
+whole_img = squeeze(six_pieces(:,:,:,1));
+while size(whole_img,1) < 145*6
+    dist = zeros(size(six_pieces, 4), 4);
+
+    top_vec = squeeze(whole_img(1,:,:));
+    bottom_vec = squeeze(whole_img(size(whole_img,1),:,:));
+    
+    for i = 1:size(six_pieces, 4)
+        vec_i_t = squeeze(six_pieces(1,:,:,i));
+        vec_i_b = squeeze(six_pieces(size(six_pieces,1),:,:,i));
+        top_dist_i_t = M_plus_S(top_vec, flip(vec_i_t,2), 1);
+        top_dist_i_b = M_plus_S(top_vec, vec_i_b, 1);
+        bottom_dist_i_t = M_plus_S(bottom_vec, vec_i_t, 1);
+        bottom_dist_i_b = M_plus_S(bottom_vec, flip(vec_i_b,2),1);
+        dist(i, 1) = top_dist_i_t;
+        dist(i, 2) = top_dist_i_b;
+        dist(i, 3) = bottom_dist_i_t;
+        dist(i, 4) = bottom_dist_i_b;
+    end
+    
+    threshold = 1e+15;
+    dist(dist>threshold) = NaN;
+    dist(dist<=0) = NaN;
+    if size(find(isnan(dist)),1)~=(size(dist,1)*size(dist,2))   % this matrix has valid values
+        [mins, indices_r] = min(dist);
+        [min_val, index_r] = min(mins);
+        col_r = index_r;
+        row_r = indices_r(index_r);
+        if size(dist,1)==1
+            row_r = 1;
+            [~, col_r] = min(dist);
+        end
+        chosen_image = squeeze(six_pieces(:, :, :, row_r));
+        six_pieces(:,:,:,row_r) = [];
+        if col_r == 1 || col_r == 4    % TT or BB
+            chosen_image = imrotate(chosen_image, 180);
+        end
+        if col_r == 1 || col_r == 2    % TT or TB
+            whole_img = cat(1, chosen_image, whole_img);
+        elseif col_r == 3 || col_r == 4 % BT or BB
+            whole_img = cat(1, whole_img, chosen_image);
+        end
+    end
+end
+
+whole_img = uint8(whole_img);
+imshow(whole_img);
