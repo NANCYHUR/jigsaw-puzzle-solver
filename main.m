e5c4e36ba1a2c476c0421f1d0e9c90a025ecf3ac
ALLEN <allen@ALLENdeMacBook-Pro.local>
2019-05-30 14:33:20 +1000

    norm the size and name

--- main.m
+++ main.m
@@ -1,27 +1,49 @@
 %Now we start to fix the Jigsaw problem
 
 %% 6*6 image
-% tic;
-% initime = cputime;
-% time1   = clock;
-% 
-% path = 'better_images/';
-% imagefiles = dir([path, '*.jpg']);
-% whole_img = Jigsaw_solver(path, imagefiles, 6, 6, 1e+11, 1e+15);
-% 
-% fintime = cputime;
-% elapsed = toc;
-% time2   = clock;
-% fprintf('TIC TOC: %g\n', elapsed);
-% fprintf('CPUTIME: %g\n', fintime - initime);
-% fprintf('CLOCK:   %g\n', etime(time2, time1));
+tic;
+initime = cputime;
+time1   = clock;
+
+path = '6by6/';
+imagefiles = dir([path, '*.jpg']);
+whole_img = Jigsaw_solver(path, imagefiles, 6, 6, 1e+11, 1e+15);
+
+fintime = cputime;
+elapsed = toc;
+time2   = clock;
+fprintf('TIC TOC: %g\n', elapsed);
+fprintf('CPUTIME: %g\n', fintime - initime);
+fprintf('CLOCK:   %g\n', etime(time2, time1));
 
 %% 7*7 image
+tic;
+initime = cputime;
+time1   = clock;
+
 path = '7by7/';
 imagefiles = dir([path, '*.jpg']);
 whole_img = Jigsaw_solver(path, imagefiles, 7, 7, 1e+11, 1e+15);
 
-% %% 8*8 image
-% path = '8by8/';
-% imagefiles = dir([path, '*.jpg']);
-% whole_img = Jigsaw_solver(path, imagefiles, 8, 8, 1e+11, 1e+15);
+fintime = cputime;
+elapsed = toc;
+time2   = clock;
+fprintf('TIC TOC: %g\n', elapsed);
+fprintf('CPUTIME: %g\n', fintime - initime);
+fprintf('CLOCK:   %g\n', etime(time2, time1));
+
+%% 8*8 image
+tic;
+initime = cputime;
+time1   = clock;
+
+path = '8by8/';
+imagefiles = dir([path, '*.jpg']);
+whole_img = Jigsaw_solver(path, imagefiles, 8, 8, 1e+11, 1e+15);
+
+fintime = cputime;
+elapsed = toc;
+time2   = clock;
+fprintf('TIC TOC: %g\n', elapsed);
+fprintf('CPUTIME: %g\n', fintime - initime);
+fprintf('CLOCK:   %g\n', etime(time2, time1));
